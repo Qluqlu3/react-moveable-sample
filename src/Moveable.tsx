09155180deccb3c7a6df330d9f203b63d12a0881
Qluqlu3 <zveupmjsgv@gmail.com>
2021-01-18 00:54:18 +0900

    update

--- src/Moveable.tsx
+++ src/Moveable.tsx
@@ -5,27 +5,31 @@ import MoveableBox from 'react-moveable';
 
 import { useInputText } from './useInputText';
 
-const Wrapper = Styled.div`
+const Wrapper = Styled.div<{ color: string }>`
+	height: 100%;
+	display: flex;
+	justify-content: start;
 	.moveable {
 		cursor: move;
+		background-color: ${(props) => '#' + props.color}
 	}
 	.moveable-control-box {
 		--moveable-color: #F00;
 		.moveable-n, .moveable-s {
 			width: 26px;
-			height: 10px;
+			height: 8px;
 			border-radius: 0;
-			margin-top: -5px;
+			margin-top: -4px;
 			margin-left: -13px;
 			background-color: #FFF;
 			border: solid 1px #F00;
 		}
 		.moveable-w, .moveable-e {
-			width: 10px;
+			width: 8px;
 			height: 26px;
 			border-radius: 0;
 			margin-top: -13px;
-			margin-left: -5px;
+			margin-left: -4px;
 			background-color: #FFF;
 			border: solid 1px #F00;
 		}
@@ -35,48 +39,87 @@ const Wrapper = Styled.div`
 			border-radius: 50%;
 			margin-left: -5px;
 		}
+		.moveable-nw, .moveable-sw, .moveable-ne, .moveable-se {
+			border: none;
+			width: 10px;
+			height: 10px;
+			border-radius: 50%;
+			margin-top: -5px;
+			margin-left: -5px;
+		}
 }`;
 
 const Box = Styled.div`
 	width: 100px;
 	height: 100px;
+	font-size: 13px;
+	font-weight: bold;
 	}
 `;
 
-const TextInput = Styled.input`
+const FormBox = Styled.div`
+	margin: 10px;
+	background-color: #DDD;
+`;
 
+const Label = Styled.label`
+	font-size: 15px;
+`;
+
+const TextInput = Styled.input`
+	width: 100px;
+	border: solid 1px #AAA;
+	border-radius: 5px;
+`;
+
+const MoveableArea = Styled.div`
+	width: 100%;
+	height: 1000px;
+	border: solid 2px #333;
 `;
 
 const Moveable: FC = () => {
   const [target, setTarget] = useState(document.querySelector('.moveable'));
   const { text, onChange } = useInputText('');
+  const { text: color, onChange: onChangeColor } = useInputText('');
 
   useEffect(() => {
     setTarget(document.querySelector('.moveable'));
   }, []);
 
   return (
-    <Wrapper>
-      <TextInput type='text' onChange={useMemo(() => onChange, [text])} />
-      <Box className={'moveable'}>{text}</Box>
-      <MoveableBox
-        //@ts-ignore
-        target={target}
-        draggable={true}
-        scalable={true}
-        rotatable={true}
-        origin={false}
-        throttleRotate={0}
-        onDrag={(e) => {
-          e.target.style.transform = e.transform;
-        }}
-        onScale={(e) => {
-          e.target.style.transform = e.transform;
-        }}
-        onRotate={(e) => {
-          e.target.style.transform = e.transform;
-        }}
-      />
+    <Wrapper color={color}>
+      <FormBox>
+        <FormBox>
+          <Label>テキスト：</Label>
+          <TextInput type='text' onChange={useMemo(() => onChange, [text])} />
+        </FormBox>
+        <FormBox>
+          <Label>背景色：#</Label>
+          <TextInput type='text' onChange={useMemo(() => onChangeColor, [color])} />
+        </FormBox>
+      </FormBox>
+      <MoveableArea>
+        <Box className={'moveable'}>{text}</Box>
+        <MoveableBox
+          //@ts-ignore
+          target={target}
+          draggable={true}
+          scalable={true}
+          rotatable={true}
+          origin={false}
+          throttleRotate={0}
+          onDrag={(e) => {
+            e.target.style.transform = e.transform;
+          }}
+          onScale={(e) => {
+            e.target.style.transform = e.transform;
+          }}
+          onRotate={(e) => {
+            e.target.style.transform = e.transform;
+          }}
+        />
+      </MoveableArea>
     </Wrapper>
   );
 };
